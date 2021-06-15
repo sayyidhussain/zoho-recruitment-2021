6750017097dc8d2dd18ced744aa8ea99e557febc
sayyidhussain <85566851+sayyidhussain@users.noreply.github.com>
2021-06-15 10:36:03 +0530

    Update PatternExam.java

--- PatternExam.java
+++ PatternExam.java
@@ -11,16 +11,7 @@ public class PatternExam {
         String[] temp=new String[val.length()];
         Arrays.fill(temp," ");
         String  splval=String.join("",temp);
-        //brute force
-        for(int i=0;i<val.length();i++){
-            for (int j = val.length(); j >= i; j--) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(val.charAt(j));
-            }
-            System.out.println();
-        }
+        
         //non brute force
         for(int i=0;i<val.length();i++){
             System.out.println(splval.substring(0,val.length()-1-i)+val.substring(0,i+1));
